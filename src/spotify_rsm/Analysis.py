9e1c1f533cd6fbab42aea3dc955cf3754e314ad7
RaminVali <60187816+RaminVali@users.noreply.github.com>
2024-02-18 21:56:30 -0500

    Modifying directory structure to get the config files working

--- src/spotify_rsm/Analysis.py
+++ src/spotify_rsm/Analysis.py
@@ -14,7 +14,7 @@ import os
 
 
 
-logging.basicConfig (level = logging.INFO, filename='logging.log')
+logging.basicConfig (level = logging.INFO, filename='logs/logging.log')
 
 
 
@@ -23,10 +23,9 @@ class Analysis:
 
         dirname = os.path.dirname(__file__)
 
-        CONFIG_PATHS = [os.path.join(dirname,'configs/system_config.yml'), 
-                        os.path.join(dirname,'configs/user_config.yml'),]
 
-        #CONFIG_PATHS = ['../../configs/system_config.yml', '../../configs/user_config.yml']
+
+        CONFIG_PATHS = ['configs/system_config.yml', 'configs/user_config.yml']
 
         # add the analysis config to the list of paths to load
         paths = CONFIG_PATHS + [analysis_config]
