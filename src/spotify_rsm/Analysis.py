37269721d9ea87261b7bc38ea7dac3d6a105daf9
RaminVali <60187816+RaminVali@users.noreply.github.com>
2024-02-18 23:17:00 -0500

    last atempt with hatchling

--- src/spotify_rsm/Analysis.py
+++ src/spotify_rsm/Analysis.py
@@ -14,7 +14,7 @@ import os
 
 
 
-logging.basicConfig (level = logging.INFO, filename='logs/logging.log')
+logging.basicConfig (level = logging.INFO, filename='logging.log')
 
 
 
@@ -25,7 +25,7 @@ class Analysis:
 
 
 
-        CONFIG_PATHS = ['configs/system_config.yml', 'configs/user_config.yml']
+        CONFIG_PATHS = ['system_config.yml', 'user_config.yml']
 
         # add the analysis config to the list of paths to load
         paths = CONFIG_PATHS + [analysis_config]
