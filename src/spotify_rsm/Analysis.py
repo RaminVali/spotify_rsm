bd474bb1649a30a5e244bc1db8c626ac0bf96a92
RaminVali <60187816+RaminVali@users.noreply.github.com>
2024-02-18 21:48:20 -0500

    trying dynamic paths to make the config fle work

--- src/spotify_rsm/Analysis.py
+++ src/spotify_rsm/Analysis.py
@@ -17,9 +17,16 @@ import os
 logging.basicConfig (level = logging.INFO, filename='logging.log')
 
 
+
 class Analysis:
     def __init__(self, analysis_config: str) -> None:
-        CONFIG_PATHS = ['../../configs/system_config.yml', '../../configs/user_config.yml']
+
+        dirname = os.path.dirname(__file__)
+
+        CONFIG_PATHS = [os.path.join(dirname,'configs/system_config.yml'), 
+                        os.path.join(dirname,'configs/user_config.yml'),]
+
+        #CONFIG_PATHS = ['../../configs/system_config.yml', '../../configs/user_config.yml']
 
         # add the analysis config to the list of paths to load
         paths = CONFIG_PATHS + [analysis_config]
