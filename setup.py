0fd00dac4eb92863a8ddcc952381256b61bd0f49
RaminVali <60187816+RaminVali@users.noreply.github.com>
2024-02-19 00:04:51 -0500

    Setuptools, includes the config files

--- setup.py
+++
@@ -1,8 +0,0 @@
-# from setuptools import setup
-
-# setup(
-#     name='spotify_rsm'
-#     version='1.1.0'
-#     author='Ramin, Sonia, Melissa' # hence the RSM
-#     packages=['spotify_rsm']
-#     )
